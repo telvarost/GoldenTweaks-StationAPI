f1ad58362d32d18716c1e19c0951f68b637e60c8
telontelvarost <telontelvarost@gmail.com>
2024-04-17 20:50:32 -0400

    UDOC-0 - Add ore mixin for gold pickaxe silk touch

---
+++ src/main/java/com/github/telvarost/goldentweaks/mixin/OreMixin.java
@@ -0,0 +1,75 @@
+package com.github.telvarost.goldentweaks.mixin;
+
+import com.github.telvarost.goldentweaks.Config;
+import net.minecraft.block.BlockBase;
+import net.minecraft.block.Ore;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.item.ItemBase;
+import net.minecraft.level.Level;
+import net.minecraft.stat.Stats;
+import org.checkerframework.common.aliasing.qual.Unique;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(Ore.class)
+public class OreMixin extends BlockBase {
+    public OreMixin(int i, int j) {
+        super(i, j, Material.STONE);
+    }
+
+    @Unique
+    private boolean brokenByGoldToolId = false;
+
+    @Unique
+    private boolean brokenByGoldToolCount = false;
+
+    @Override
+    public void afterBreak(Level arg, PlayerBase player, int i, int j, int k, int l) {
+        System.out.println("Hello I here");
+        if (Config.config.enableGoldPickaxeSilkTouch) {
+            brokenByGoldToolId = false;
+            brokenByGoldToolCount = false;
+
+            if (  (null != player)
+                    && (null != player.inventory)
+                    && (null != player.inventory.getHeldItem())
+                    && (ItemBase.goldPickaxe.id == player.inventory.getHeldItem().itemId)
+            ) {
+                brokenByGoldToolId = true;
+                brokenByGoldToolCount = true;
+            }
+        }
+
+        player.increaseStat(Stats.mineBlock[this.id], 1);
+        this.drop(arg, i, j, k, l);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getDropId", cancellable = true)
+    public void goldenTweaks_getDropId(int i, Random random, CallbackInfoReturnable<Integer> cir) {
+        if (!Config.config.enableGoldPickaxeSilkTouch) {
+            return;
+        }
+
+        if (brokenByGoldToolId) {
+            cir.setReturnValue(id);
+            brokenByGoldToolId = false;
+        }
+    }
+
+    @Inject(at = @At("HEAD"), method = "getDropCount", cancellable = true)
+    public void getDropCount(Random random, CallbackInfoReturnable<Integer> cir) {
+        if (!Config.config.enableGoldPickaxeSilkTouch) {
+            return;
+        }
+
+        if (brokenByGoldToolCount) {
+            cir.setReturnValue(1);
+            brokenByGoldToolCount = false;
+        }
+    }
+}
