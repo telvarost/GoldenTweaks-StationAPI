3b0b26e3fe5dd81f5a7f2323ed29619f9fb39854
telontelvarost <telontelvarost@gmail.com>
2024-04-18 19:12:42 -0400

    UDOC-0 - Add compatibility for AnnoyanceFix

--- src/main/java/com/github/telvarost/goldentweaks/mixin/BookshelfMixin.java
+++ src/main/java/com/github/telvarost/goldentweaks/mixin/BookshelfMixin.java
@@ -6,13 +6,13 @@ import net.minecraft.block.Bookshelf;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerBase;
 import net.minecraft.item.ItemBase;
+import net.minecraft.item.ItemInstance;
 import net.minecraft.level.Level;
 import net.minecraft.stat.Stats;
 import org.checkerframework.common.aliasing.qual.Unique;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
@@ -24,24 +24,51 @@ public class BookshelfMixin extends BlockBase {
     }
 
     @Unique
-    private boolean brokenByGoldTool = false;
+    private boolean brokenByGoldToolId = false;
+
+    @Unique
+    private boolean brokenByGoldToolCount = false;
 
     @Override
     public void afterBreak(Level arg, PlayerBase player, int i, int j, int k, int l) {
 
         if (Config.config.enableGoldAxeSilkTouch) {
-            brokenByGoldTool = false;
+            brokenByGoldToolId = false;
+            brokenByGoldToolCount = false;
 
             if (  (null != player)
                 && (null != player.inventory)
                 && (null != player.inventory.getHeldItem())
                 && (ItemBase.goldAxe.id == player.inventory.getHeldItem().itemId)
             ) {
-                brokenByGoldTool = true;
+                brokenByGoldToolId = true;
+                brokenByGoldToolCount = true;
             }
 
             player.increaseStat(Stats.mineBlock[this.id], 1);
             this.drop(arg, i, j, k, l);
+        }
+    }
+
+    @Override
+    public void beforeDestroyedByExplosion(Level arg, int i, int j, int k, int l, float f) {
+        if (!arg.isServerSide) {
+            int var7 = this.getDropCount(arg.rand);
+
+            for(int var8 = 0; var8 < var7; ++var8) {
+                if (!(arg.rand.nextFloat() > f)) {
+                    int var9 = this.getDropId(l, arg.rand);
+
+                    if (brokenByGoldToolId) {
+                        brokenByGoldToolId = false;
+                        var9 = BlockBase.BOOKSHELF.id;
+                    }
+
+                    if (var9 > 0) {
+                        this.drop(arg, i, j, k, new ItemInstance(var9, 1, this.droppedMeta(l)));
+                    }
+                }
+            }
         }
     }
 
@@ -51,9 +78,9 @@ public class BookshelfMixin extends BlockBase {
             return;
         }
 
-        if (brokenByGoldTool) {
+        if (brokenByGoldToolCount) {
             cir.setReturnValue(1);
-            brokenByGoldTool = false;
+            brokenByGoldToolCount = false;
         }
     }
 }
