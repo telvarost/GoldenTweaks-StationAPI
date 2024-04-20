c63b74f3e3845302e7041a42b7a97d5afa4c7ac7
telontelvarost <telontelvarost@gmail.com>
2024-04-19 20:06:25 -0400

    UDOC-0 - Remove disused variable

--- src/main/java/com/github/telvarost/goldentweaks/ModHelper.java
+++ src/main/java/com/github/telvarost/goldentweaks/ModHelper.java
@@ -3,7 +3,6 @@ package com.github.telvarost.goldentweaks;
 public class ModHelper {
 
     public static class ModHelperFields {
-        public static Integer NumberOfKills = 0;
         public static Integer UsingGoldSword = 0;
     }
 }
