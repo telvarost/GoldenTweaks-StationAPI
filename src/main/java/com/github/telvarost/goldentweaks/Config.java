8284bd72a7287beace5c4b2a88ea6e89ae3cbede
telvarost <telontelvarost@gmail.com>
2024-11-04 19:38:34 -0500

    Migrate to GCAPI3

--- src/main/java/com/github/telvarost/goldentweaks/Config.java
+++ src/main/java/com/github/telvarost/goldentweaks/Config.java
@@ -1,91 +1,103 @@
 package com.github.telvarost.goldentweaks;
 
-import blue.endless.jankson.Comment;
-import net.glasslauncher.mods.api.gcapi.api.*;
+import net.glasslauncher.mods.gcapi3.api.*;
 
 public class Config {
 
-    @GConfig(value = "config", visibleName = "GoldenTweaks")
+    @ConfigRoot(value = "config", visibleName = "GoldenTweaks")
     public static ConfigFields config = new ConfigFields();
 
     public static class ConfigFields {
 
-        @ConfigName("Enable Fishing Rod Luck Of The Sea")
-        @Comment("10% chance to catch a gold ingot with a fish")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Fishing Rod Luck Of The Sea",
+                description = "10% chance to catch a gold ingot with a fish",
+                multiplayerSynced = true
+        )
         public Boolean enableFishingRodLuckOfTheSea = true;
 
-        @ConfigName("Enable Gold Apple Cure Fire")
-        @Comment("Cancels fire effect one time when eaten")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Apple Cure Fire",
+                description = "Cancels fire effect one time when eaten",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldAppleCureFire = true;
 
-        @ConfigName("Enable Gold Axe Silk Touch")
-        @Comment("Tool must be effective on block for effect")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Axe Silk Touch",
+                description = "Tool must be effective on block for effect",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldAxeSilkTouch = true;
 
-        @ConfigName("Enable Gold Block Herobrine Summoner")
-        @Comment("Converts a gold block into a sponge when lit")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Block Herobrine Summoner",
+                description = "Converts a gold block into a sponge when lit",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldBlockHerobrineSummoner = true;
 
-        @ConfigName("Enable Gold Boots Soul Speed")
-        @Comment("Negates the slow-down effect of soul sand")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Boots Soul Speed",
+                description = "Negates the slow-down effect of soul sand",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldBootsSoulSpeed = true;
 
-        @ConfigName("Enable Gold Chestplate Projectile Protection")
-        @Comment("Only reduces skeleton arrow damage")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Chestplate Projectile Protection",
+                description = "Only reduces skeleton arrow damage",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldChestplateProjectileProtection = true;
 
-        @ConfigName("Enable Gold Helmet Blast Protection")
-        @Comment("Only reduces creeper explosion damage")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Helmet Blast Protection",
+                description = "Only reduces creeper explosion damage",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldHelmetBlastProtection = true;
 
-        @ConfigName("Enable Gold Hoe Fortune")
-        @Comment("Fortune is just x2 on crops and flint")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Hoe Fortune",
+                description = "Fortune is just x2 on crops and flint",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldHoeFortune = true;
 
-        @ConfigName("Enable Gold Leggings Thorns")
-        @Comment("Only damages attacking zombies and spiders")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Leggings Thorns",
+                description = "Only damages attacking zombies and spiders",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldLeggingsThorns = true;
 
-        @ConfigName("Enable Gold Pickaxe Silk Touch")
-        @Comment("Tool must be effective on block for effect")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Pickaxe Silk Touch",
+                description = "Tool must be effective on block for effect",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldPickaxeSilkTouch = true;
 
-        @ConfigName("Enable Gold Shovel Silk Touch")
-        @Comment("Tool must be effective on block for effect")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Shovel Silk Touch",
+                description = "Tool must be effective on block for effect",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldShovelSilkTouch = true;
 
-        @ConfigName("Enable Gold Sword Looting")
-        @Comment("Looting is a simple +1 to mob drops")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Gold Sword Looting",
+                description = "Looting is a simple +1 to mob drops",
+                multiplayerSynced = true
+        )
         public Boolean enableGoldSwordLooting = true;
 
-        @ConfigName("Enable Sponge Soaking Up Water")
-        @Comment("Will soak up a 5x5 water cube when placed")
-        @MultiplayerSynced
-        @ValueOnVanillaServer(booleanValue = TriBoolean.FALSE)
+        @ConfigEntry(
+                name = "Enable Sponge Soaking Up Water",
+                description = "Will soak up a 5x5 water cube when placed",
+                multiplayerSynced = true
+        )
         public Boolean enableSpongeSoaksUpWater = true;
     }
 }
